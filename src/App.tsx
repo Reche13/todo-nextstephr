d276336605db913fe86dd5325afec9959e9d7dac
Reche Soares <rechesoares13@gmail.com>
2026-02-01 17:36:35 +0530

    add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 function App() {
-  return <div className="">hello world</div>;
+  return (
+    <div className="bg-red-300 w-screen h-screen text-3xl">hello world</div>
+  );
 }
 
 export default App;
